83e60e6653f6802a4be229a22af0beca994b97ff
ikserm <iksermd@gmail.com>
2021-01-15 18:00:52 -0600

    Add Articles component, add components to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from "./components/home/home.component";
+import { ArticlesComponent } from "./components/articles/articles.component";
+
+const routes: Routes = [
+  { path:"home",  component: HomeComponent},
+  { path:"articles",  component: ArticlesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { ArticlesComponent } from './components/articles/articles.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    ArticlesComponent
   ],
   imports: [
     BrowserModule,
