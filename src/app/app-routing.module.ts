ad279437c6bb22d329dabf37818663b58b6efdea
ikserm <iksermd@gmail.com>
2021-01-16 17:13:22 -0600

    Routing ++
    Prevents to introduce wrong paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from "./components/home/home.component";
 import { ArticlesComponent } from "./components/articles/articles.component";
 
+import { OktaCallbackComponent, OktaAuthGuard } from '@okta/okta-angular';
+
 const routes: Routes = [
   { path:"home",  component: HomeComponent},
-  { path:"articles",  component: ArticlesComponent}
+  { path:"articles",  component: ArticlesComponent},
+  { path: 'implicit/callback', component: OktaCallbackComponent },
+  { path: '**', pathMatch: 'full',  redirectTo:'home'}
 ];
 
 @NgModule({
