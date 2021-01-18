8f041aafef4de78eefb3b2dbce4c7036e595ea6c
ikserm <iksermd@gmail.com>
2021-01-17 23:09:07 -0600

    Add funtions to show articles from the database

--- src/app/components/articles/articles.component.ts
+++ src/app/components/articles/articles.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ServerService } from "../../services/server.service";
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser'
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
@@ -8,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticlesComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  articles: any[] = [];
+  currentArticle: any = {id: null, name: '', description:'', state:'', date:new Date()};
+
+  constructor(  private formBuildeer: FormBuilder,
+                private server: ServerService) { }
 
   ngOnInit(): void {
+    this.form =this.formBuildeer.group({
+      name: [this.currentArticle.name, Validators.required],
+      description: this.currentArticle,
+      state: this.currentArticle,
+      date: [this.currentArticle, Validators.required]
+    });
+    this.getArticles();
+  }
+
+  private getArticles(){
+    this.server.getArticles().then((response: any) => {
+      console.log('Response', response);
+      this.articles = response.map((ar) => {
+        return ar;
+      });
+    });
   }
 
 }
